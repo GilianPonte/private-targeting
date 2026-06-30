6943fdd5e96e00bb8e8268e3d34e20664888b7e7
Gilian <gilianponte@live.nl>
2026-06-30 13:18:44 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-from private_targeting import __version__, cnn, ctenn, dp_cate, pcnn
+from private_targeting import __version__, CTENN, DP_CATE, DP_policy
 
 
 def test_version_is_present():
@@ -6,9 +6,7 @@ def test_version_is_present():
     assert __version__
 
 
-
-def test_aliases_exist():
-    assert callable(cnn)
-    assert callable(pcnn)
-    assert callable(ctenn)
-    assert callable(dp_cate)
+def test_public_api_exists():
+    assert callable(CTENN)
+    assert callable(DP_CATE)
+    assert callable(DP_policy)
